27d0d3daea507eda8fce5368630909d6c350d824
maxwellpv <55470371+maxwellpv@users.noreply.github.com>
2022-12-07 17:33:17 -0500

    Implement "Read" function of CRUD.

--- src/app/annual-records-view/annual-records-view.component.ts
+++ src/app/annual-records-view/annual-records-view.component.ts
@@ -10,9 +10,10 @@ import {AnnualRecordService} from "../services/annual-record.service";
   styleUrls: ['./annual-records-view.component.css']
 })
 export class AnnualRecordsViewComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  displayedColumns: string[] = ['year', 'state', 'category', 'item', 'amount', 'gdp'];
   annualRecords!: any[];
+  dataSource = new MatTableDataSource<AnnualRecord>(this.annualRecords);
+
 
   @ViewChild(MatPaginator) paginator: MatPaginator | any;
 
@@ -24,6 +25,7 @@ export class AnnualRecordsViewComponent implements OnInit {
     annualRecordService.getAllAnnualRecords().subscribe(
       data => {
         this.annualRecords = data;
+        this.dataSource = new MatTableDataSource<AnnualRecord>(this.annualRecords);
         console.log(data);
       }
     )
@@ -34,32 +36,35 @@ export class AnnualRecordsViewComponent implements OnInit {
 
 }
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+export interface AnnualRecord {
+  id: number;
+  year: string;
+  gdp: number;
+  amount: number;
+  item: string;
+  state: number;
+  category: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+//   {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
+//   {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
+//   {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
+//   {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
+//   {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
+//   {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
+//   {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
+//   {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
+//   {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
+//   {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
+// ];
